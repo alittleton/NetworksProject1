2b1bee54e78a3573b7cbdb7449c93fd4f660a27f
Andrew Littleton <andrewlittleton@charter.net>
2016-02-14 18:17:22 -0600

    removing C files

--- server.cpp
+++ server.cpp
@@ -11,9 +11,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h> 
+//#include <sys/types.h>
+//#include <sys/socket.h>
+#include <winsock2.h>
+//#include <netinet/in.h> 
 
 using namespace std;
 
